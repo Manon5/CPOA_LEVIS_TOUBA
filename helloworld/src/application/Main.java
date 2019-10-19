5c0ab6a86ddbee90dd21e12938b88c7a0f15e51f
Manon5 <manon.touba05@gmail.com>
2019-10-19 11:50:30 +0200

    jydjx ntm

--- helloworld/src/application/Main.java
+++ helloworld/src/application/Main.java
@@ -1,28 +1,39 @@
 package application;
 	
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 
 
-public class Main extends Application {
+public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/fenetre.fxml"));
-			Scene scene = new Scene(root,600,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+		URL fxmlURL=getClass().getResource("/sources/fenetre.fxml");
+		FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+		Node root = fxmlLoader.load();
+		Scene scene = new Scene((VBox) root, 600, 400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Ma première fenêtre JavaFX");
+		primaryStage.show();
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
+			}
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
