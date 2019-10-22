c4eb9a42888d94f9bad0e2371bfe652dc69af56c
Manon <Manon@DESKTOP-PK67DQJ>
2019-10-22 20:21:56 +0200

    ajout du panneau de saisie client (ne compile pas : pourquoi ?)

--- helloworld/src/application/Main.java
+++ helloworld/src/application/Main.java
@@ -5,35 +5,42 @@ import java.awt.event.ActionListener;
 import java.net.URL;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 
 
 public class Main extends Application{
+	
+	@FXML
+	private Label id_lb_custom;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-		URL fxmlURL=getClass().getResource("/sources/fenetre.fxml");
+		try {URL fxmlURL=getClass().getResource("/sources/saisir_client.fxml");
 		FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
 		Node root = fxmlLoader.load();
 		Scene scene = new Scene((VBox) root, 600, 400);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Ma premiÃ¨re fenÃªtre JavaFX");
+		primaryStage.setTitle("Ma première fenêtre JavaFX");
 		primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 			}
-	}
+		}
 	
-	public static void main(String[] args) {
-		launch(args);
+	public void creerModele() {
+		
+		
+		this.id_lb_custom.setText("test");
 	}
 
 }
