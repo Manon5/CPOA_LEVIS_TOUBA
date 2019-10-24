483f00b42fbd4035b57aca80f882ca0f2e757554
Manon5 <manon.touba05@gmail.com>
2019-10-24 20:21:31 +0200

    Update Main.java

--- helloworld/src/application/Main.java
+++ helloworld/src/application/Main.java
@@ -24,13 +24,13 @@ public class Main extends Application{
 	
 	@Override
 	public void start(Stage primaryStage) {
-		try {URL fxmlURL=getClass().getResource("/sources/saisir_client.fxml");
+		try {URL fxmlURL=getClass().getResource("/sources/accueil_temp.fxml");
 		FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
 		Node root = fxmlLoader.load();
 		Scene scene = new Scene((VBox) root, 600, 400);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Ma première fenêtre JavaFX");
+		primaryStage.setTitle("Ma premiÃ¨re fenÃªtre JavaFX");
 		primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
