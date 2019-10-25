531230e36160c4fb0fadeda8980d0cdd14c22a29
Manon5 <manon.touba05@gmail.com>
2019-10-25 16:00:30 +0200

    ajout boutons retour

--- helloworld/src/FxControleurs/AjoutPeriodCtrl.java
+++ helloworld/src/FxControleurs/AjoutPeriodCtrl.java
@@ -1,5 +1,11 @@
 package FxControleurs;
 
+import FxVues.AjoutPeriodVue;
+import FxVues.AjoutRevueVue;
+
 public class AjoutPeriodCtrl {
 
+	public void setVue(AjoutPeriodVue V) {
+		// TODO Stub de la méthode généré automatiquement
+	}
 }
--- helloworld/src/FxVues/AjoutPeriodVue.java
+++ helloworld/src/FxVues/AjoutPeriodVue.java
@@ -1,5 +1,34 @@
 package FxVues;
+	
+import java.net.URL;
 
-public class AjoutPeriodVue {
+import FxControleurs.AjoutAboCtrl;
+import FxControleurs.AjoutPeriodCtrl;
+import FxControleurs.AjoutRevueCtrl;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+
+public class AjoutPeriodVue extends Stage {
+	
+	
+	public AjoutPeriodVue() {
+		try {
+			final URL fxmlURL= getClass().getResource("/sources/saisir_period.fxml");
+	        this.setTitle("Saisir un abonnement");
+			final FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL); 
+			final VBox node = (VBox)fxmlLoader.load();
+	        this.setScene(new Scene(node, 600, 400));
+	        AjoutPeriodCtrl controleur = fxmlLoader.getController();
+	        controleur.setVue(this);
+			this.initModality(Modality.APPLICATION_MODAL);
+	        this.show();
+		}catch(Exception e){
+			
+		}
+}
+
 
 }
